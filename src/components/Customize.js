53032044296f2f55a953b19227e55f539c70e023
karthikt <karthik.thatipelly96@gmail.com>
2022-04-14 16:20:03 +0530

    homepage updated

--- src/components/Customize.js
+++ src/components/Customize.js
@@ -1,49 +1,65 @@
 function Customize() {
     return (
-        <div className='container p-5'>
-         <h1>MAKE YOUR PIZZA</h1>
-         <p>-------*-------</p>
-         <div>
-         <h3>TYPE</h3>
-         <input type="checkbox" id="veg" name="veg"/>
-         <label for="veg">Veg Supreme Pizza</label>
-         <input type="checkbox" id="nonveg" name="veg"/>
-         <label for="nonveg">nonVeg Supreme Pizza</label>
+        <div className='container p-5 '>
+            
+            <h1>MAKE YOUR PIZZA</h1>
+            <p>-------*-------</p>
+           <form >
+            <div class="form-group row">
+            <label for="size" class="col-4 col-form-label">Size</label>
+                <select class="form-control col-8" id="size">
+                    <option>Select One</option>
+                    <option>Small</option>
+                    <option>Medium</option>
+                    <option>Large</option>
+                </select>
+            </div>
+            <div class="form-group row">
+            <label for="crusts" class="col-4 col-form-label">Crusts</label>
+                <select class="form-control col-8" id="crusts">
+                    <option>Select One</option>
+                    <option>Pan</option>
+                    <option>Thin Crust</option>
+                    <option>Cheeseburst</option>
+                </select>
+            </div>
+            <div class="form-group row">
+            <label for="cheese" class="col-4 col-form-label">Cheese</label>
+                <select class="form-control col-8" id="cheese">
+                    <option>Select One</option>
+                    <option>Parmesan</option>
+                    <option>Mozzarella</option>
+                    <option>Pecorino</option>
+                    <option>Asiago</option>
+                </select>
+            </div>
+            <div class="form-group row">
+            <label for="vegtoppings" class="col-4 col-form-label">Veg-Toppings</label>
+                <select class="form-control col-8" id="vegtoppings">
+                    <option>Select One</option>
+                    <option>Black Olives</option>
+                    <option>Tomato</option>
+                    <option>Mushroom</option>
+                    <option>Babycorn</option>
+                    <option>Green Chillies</option>
+                </select>
+            </div>
+            <div class="form-group row">
+            <label for="nonvegtoppings" class="col-4 col-form-label">NonVeg-Toppings</label>
+                <select class="form-control col-8" id="nonvegtoppings">
+                    <option>Select One</option>
+                    <option>Chicken Chunks</option>
+                    <option>BBQ chicken</option>
+                    <option>Chicken Salami</option>
+                </select>
+            </div>
+            </form>
 
-         </div>
-         <div>
-         <h3>Bases/crusts</h3>
-         <input type="checkbox" id="thin" name="thin"/>
-         <label for="thin">thin crust</label>
-         <input type="checkbox" id="cheeseburst" name="cheeseburst"/>
-         <label for="cheeseburst">cheeseburst</label>
-         <input type="checkbox" id="pan" name="pan"/>
-         <label for="pan">pan</label>
+            <button className="btn btn-warning">Add to cart</button>
 
-         </div>
-         <div>
-         <h3>Toppings</h3>
-         <input type="checkbox" id="Blackolives" name="Blackolives"/>
-         <label for="Blackolives">Blackolives crust</label>
-         <input type="checkbox" id="Tomato" name="Tomato"/>
-         <label for="Tomato">Tomato</label>
-         <input type="checkbox" id="Mushroom" name="Mushroom"/>
-         <label for="Mushroom">Mushroom</label>
 
-         <input type="checkbox" id="Blackolives" name="Blackolives"/>
-         <label for="Blackolives">Blackolives crust</label>
-         <input type="checkbox" id="Tomato" name="Tomato"/>
-         <label for="Tomato">Tomato</label>
-         <input type="checkbox" id="Mushroom" name="Mushroom"/>
-         <label for="Mushroom">Mushroom</label>
 
-         </div>
-
-         <button className="btn">Add to cart</button>
-         
-         
-
-      </div>
+        </div>
     );
 }
 

d18d24a92d71f7d74ffee4b6352ff9ba52b0df3e
karthikt <karthik.thatipelly96@gmail.com>
2022-04-20 18:21:00 +0530

    layout and order placing modules changed

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,16 +1,81 @@
+import Axios from 'axios';
+
+
+//let size,crust,cheese, vegtoppings, nonvegtoppings;
+let sizeCost, crustCost,cheeseCost, vegtoppingsCost, nonvegtoppingsCost,totalCost;
+let amount;
 function Checkout() {
+
+    const total =(cost) => {
+          amount = cost;
+    }
+
+    const showSummary = () => {
+        Axios.get("http://localhost:3001/checkout", {
+            params: {
+                id:1
+            }
+        })
+          .then(response => {
+              console.log(response); 
+              switch(response.data[0].size){
+                  case 'small':
+                      sizeCost = 100;
+                      break;
+                  case 'medium':
+                      sizeCost = 125;
+                      break;
+                   default:
+                       sizeCost = 150;
+                       break;       
+              }
+              switch(response.data[0].crust){
+                  case 'pan' :
+                      crustCost = 0;
+                      break;
+                  case 'thincrust':
+                      crustCost = 25;
+                      break;
+                   default:
+                       crustCost = 50;
+                       break;       
+              }
+              switch(response.data[0].cheese){
+                  case 'mozzarella':
+                      cheeseCost =0;
+                      break;
+                  case 'asiago' :
+                      cheeseCost = 40;
+                      break;
+                  case 'parmesan' :
+                        cheeseCost = 40;
+                        break;
+                  default :
+                        cheeseCost = 40;
+                         break;         
+
+              }
+              vegtoppingsCost = 40;
+              if(response.data[0].nonvegtoppings !== 0)
+              nonvegtoppingsCost = 40;
+
+              totalCost = sizeCost + crustCost +cheeseCost + vegtoppingsCost + nonvegtoppingsCost;
+              console.log(totalCost);
+              total(totalCost);
+
+          })
+    }
     return(
         <div className="p-5">
-    <h1>Order Summary</h1>
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-    <h2>Total:</h2>
-    <button className="btn btn-danger">please Login to purchase</button>
+           <h3>Order Summary</h3>
+           
+           <button onClick={showSummary}>Calculate</button>
+           
+       
+           <h2>{amount}</h2>
+    {/*<button className="btn btn-danger">please Login to purchase</button> */}
     <button className="btn btn-success">Place order</button>
-    <a href="#">View previous Orders</a>
+    {/*<a href="#">View previous Orders</a>*/}
     </div>
     );
 }
--- src/components/Customize.js
+++ src/components/Customize.js
@@ -1,72 +1,86 @@
-let vegToppings = ['blackolives', 'tomato','mushrooms', 'babycorn', 'greenchillies']
-let nvegToppings = ['chickenchunks', 'chickensalami', 'bbqchicken']
+import React, {useState} from 'react';
+import Axios from 'axios';
+
 function Customize() {
-    return (
-        <div className='container p-5 '>
+    const[size, setSize] = useState("");
+    const[crust, setCrust] = useState("");
+    const[cheese, setCheese] = useState("");
+    const [vegToppings, setVegToppings] = useState("")
+    const [nonvegToppings, setNonvegToppings] = useState("");
 
-            <h1>MAKE YOUR PIZZA</h1>
-            <p>-------*-------</p>
-            <form method="post" action=" ">
-                <div class="form-group row ">
-                    <label for="size" class="col-4 col-form-label">Size</label>
-                    <select class="form-control col-5" id="size">
-                        <option>Select One</option>
-                        <option>Small</option>
-                        <option>Medium</option>
-                        <option>Large</option>
-                    </select>
-                </div>
-                <div class="form-group row">
-                    <label for="crusts" class="col-4 col-form-label">Crusts</label>
-                    <select class="form-control col-5" id="crusts">
-                        <option>Select One</option>
-                        <option>Pan</option>
-                        <option>Thin Crust</option>
-                        <option>Cheeseburst</option>
-                    </select>
-                </div>
-                <div class="form-group row">
-                    <label for="cheese" class="col-4 col-form-label">Cheese</label>
-                    <select class="form-control col-5" id="cheese">
-                        <option>Select One</option>
-                        <option>Parmesan</option>
-                        <option>Mozzarella</option>
-                        <option>Pecorino</option>
-                        <option>Asiago</option>
-                    </select>
-                </div>
-                <div class="form-group row">
-                    <div class="col-4">Veg-Toppings</div>
-                    <div class="col-1">
-                       {vegToppings.map(vtopping => {
-                           return ( <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id={vtopping}/>
-                                <label class="form-check-label" for={vtopping}>
-                                    {vtopping}
-                                </label>
-                        </div>)
-                       })}
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-4">NonVeg-Toppings</div>
-                    <div class="col-1">
-                       {nvegToppings.map(nvtopping => {
-                           return ( <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id={nvtopping}/>
-                                <label class="form-check-label" for={nvtopping}>
-                                   {nvtopping}  
-                                </label>
-                        </div>)
-                       })}
-                    </div>
-                </div>
-            </form>
+    //form submit
+    const handleCart =() => {
+        Axios.post('http://localhost:3001/', {
+            size: size,
+            crust: crust,
+            cheese: cheese,
+            vegtoppings: vegToppings,
+            nonvegtoppings: nonvegToppings
+        }).then ((response) => {
+            console.log(response);
+         })
+    }
 
-            <button className="btn btn-warning" type="submit">Add to cart</button>
-
-
-
+    return(
+        <div className="container m-auto p-5">
+            <div>
+                <h2>MAKE YOUR PIZZA</h2>
+                <p>---------^--------</p>
+            </div>
+            <div>
+                {/*size */}
+                <label>Choose Size:   </label>
+                <input type="text" 
+                       onChange={(e) => {
+                        setSize(e.target.value)
+                        }}
+                       placeholder = "Type something" 
+                       className="m-3 p-2"
+                 /><br />
+                <p><em>(small : 100rs | medium: 125rs | large: 150rs )</em></p><br/>
+                {/* crust*/}
+                <label>Choose Crust:   </label>
+                <input type="text" 
+                       onChange={(e) => {
+                        setCrust(e.target.value)
+                        }}
+                       placeholder = "Type something" 
+                       className="m-3 p-2"
+                 /><br />
+                <p><em>(pan : +0rs | thincrust: +25rs | cheeseburst: +50rs )</em></p><br/>
+                {/* cheese*/}
+                <label>Choose Cheese:   </label>
+                <input type="text" 
+                       onChange={(e) => {
+                        setCheese(e.target.value)
+                        }}
+                       placeholder = "Type something" 
+                       className="m-3 p-2"
+                 /><br />
+                <p><em>(Mozzarella : +0rs | Asiago: +40rs | Parmesan: +40rs | Pecorino: +40rs )</em></p><br/>
+                {/* Veg toppings*/}
+                <label>Veg Toppings:   </label>
+                <input type="text" 
+                       onChange={(e) => {
+                        setVegToppings(e.target.value)
+                        }}
+                       placeholder = "Type something" 
+                       className="m-3 p-2"
+                 /><br />
+                <p><em>(choose any 2 [each +20rs]: blackolives, tomatoes, mushrooms, babycorn, greenchillies  )</em></p><br/>
+                 {/* Veg toppings*/}
+                 <label>NonVeg Toppings:   </label>
+                <input type="text" 
+                       onChange={(e) => {
+                        setNonvegToppings(e.target.value)
+                        }}
+                       placeholder = "Type something" 
+                       className="m-3 p-2"
+                 /><br />
+                <p><em>(choose any 1 [each +40rs] (optional): chickenchunks, chickensalami, bbqchicken )</em></p><br/>
+                
+                <button className='btn btn-warning m-2' onClick={handleCart}>Add to Cart</button>
+            </div>
         </div>
     );
 }
